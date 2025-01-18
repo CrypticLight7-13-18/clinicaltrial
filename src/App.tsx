b1d1e132de439ed7947f1f2c0084fc32506cdaf9
CrypticLight7-13-18 <lightu7.13.18@gmail.com>
2025-01-18 15:43:41 +0530

    minor

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AuthPages from "./pages/auth";
-import CreateTrialPage from './pages/addTrial'
-import AddParticipantPage from './pages/addParticipant'
-import TrialDashboard from './pages/trials'
+import CreateTrialPage from "./pages/addTrial";
+import AddParticipantPage from "./pages/addParticipant";
+import TrialDashboard from "./pages/trials";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-        <Route
-              path="/"
-              element={
-                <TrialDashboard />
-              }
-            />        </Routes>
+          <Route path="/" element={<AuthPages />} />
+          <Route path="/trials" element={<TrialDashboard />} />
+          <Route path="/add-trial" element={<CreateTrialPage />} />
+          <Route path="/add-participant" element={<AddParticipantPage />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
